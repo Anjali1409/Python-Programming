d1b428db306569973506e653546873fa633c66d5
Anjali1409 <36623776+Anjali1409@users.noreply.github.com>
2018-02-20 11:25:45 +0530

    Update arithmetic operations.py

--- Beginner Level/arithmetic operations.py
+++ Beginner Level/arithmetic operations.py
@@ -1,13 +1,13 @@
-a=10;
-b=5;
+a=10
+b=5
 c=int(a)+int(b)
 print('sum of {0} and {1} is {2}'.format(a,b,c))
-d=15;
-e=2;
+d=15
+e=2
 f=int(d)*int(e)
 print('multiplication of {0} and {1} is {2}'.format(d,e,f))
-g=35;
-h=7;
+g=35
+h=7
 i=int(g)/int(h)
 print('division of {0} and {1} is {2}'.format(g,h,i))
 j=int(f)-int(i)
