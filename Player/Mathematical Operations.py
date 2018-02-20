15ed2c0b94772cdc8e30c437ac0bb56778bcd025
Anjali1409 <36623776+Anjali1409@users.noreply.github.com>
2018-02-20 12:00:54 +0530

    Create Mathematical Operations.py

---
+++ Player/Mathematical Operations.py
@@ -0,0 +1,16 @@
+ a=10
+ b=5
+ c=int(a)+int(b)
+ d=int(a)-int(b)
+ e=int(a)*int(b)
+ f=int(a)/int(b)
+ g=int(a)**int(b)
+ h=int(a)%int(b)
+ print "a=10"
+ print "b=5"
+ print "sum:",c
+ print "difference:",d
+ print "multiplication:",e
+ print "division:",f
+ print "exponential:",g
+ print "remainder:",h
