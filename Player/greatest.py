7d0ed38687d23727b3b8d77fc98a62147a37ab36
Anjali1409 <36623776+Anjali1409@users.noreply.github.com>
2018-05-16 15:26:25 +0530

    Update greatest.py

--- Player/greatest.py
+++ Player/greatest.py
@@ -1,10 +1,9 @@
 i=int(input())
 j=int(input())
 k=int(input())
-if(i>j):
-	if(i>k):
-		print(i)
-	elif(j>k):
-		print(j)
+if(i>j and i>k):
+	print(i)
+elif(j>i and j>k):
+	print(j)
 else:
 	print(k)
