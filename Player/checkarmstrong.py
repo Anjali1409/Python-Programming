ce6eaad904b08c31a82cb1c0a241c58f37234877
Anjali1409 <36623776+Anjali1409@users.noreply.github.com>
2018-05-19 15:27:14 +0530

    Update checkarmstrong.py

--- Player/checkarmstrong.py
+++ Player/checkarmstrong.py
@@ -1,12 +1,15 @@
 a=int(input())
-sum=0
-t=a
-while(t>0):
-    digit=t%10
-    sum+=digit**3
-    t//=10
-if(a==sum):
-    print("yes")
-else:
-    print("no")
+if(a<=100000):
+    sum=0
+    t=a
+    while(t>0):
+        digit=t%10
+        sum+=digit**3
+        t//=10
+    if(a==sum):
+        print("yes")
+    else:
+        print("no")
+            
+
                 
