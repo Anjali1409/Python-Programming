bb7826fb10c0175f79a248724d91993c156cc1aa
Anjali1409 <36623776+Anjali1409@users.noreply.github.com>
2018-05-20 19:11:42 +0530

    Update check n is power of 2.py

--- Player/check n is power of 2.py
+++ Player/check n is power of 2.py
@@ -1,7 +1,8 @@
 a=int(input())
-for i in range(a):
-    if(a==i**2):
-        print('yes')
-        break
+if(a>=1 and a<=100000):
+    for i in range(a):
+        if(a==i**2):
+            print('yes')
+            break
 else:
     print('no')
