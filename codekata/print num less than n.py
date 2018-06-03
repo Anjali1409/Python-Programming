cffa429c0367e3fda084cd93fe39e5d18b942dbe
Anjali1409 <36623776+Anjali1409@users.noreply.github.com>
2018-06-03 17:01:19 +0530

    Update print num less than n.py

--- codekata/print num less than n.py
+++ codekata/print num less than n.py
@@ -1,7 +1,8 @@
 n=int(input())
-ni=(list(map(int,input().split())))
-li=[]
-for i in ni:
-    if(i<n):
-        li.append(i)
+if(n>=1 and n<=10000):
+    ni=(list(map(int,input().split())))
+    li=[]
+    for i in ni:
+        if(i<n):
+            li.append(i)
 print(sorted(li))
